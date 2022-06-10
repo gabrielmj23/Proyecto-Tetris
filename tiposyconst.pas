9660410cf083387d19bea21908eae6cbcbaede8e
gabrielmj23 <gabrielmj2312@gmail.com>
2022-06-09 23:18:13 -0400

    Unidad con tipos y constantes de uso general en el proyecto

---
+++ tiposyconst.pas
@@ -0,0 +1,37 @@
+unit tiposYConst;
+
+// Público ---------------------------------------------------------------------
+interface
+  type
+    Juego = Record
+              Codigo: DWord;
+              Usuario: String[12];
+              PtsGanados: DWord;
+            end;
+
+    Jugador = Record
+                NombreComp: String[25];
+                Usuario: String[12];
+                Clave: String[10];
+                Correo: String[30];
+                IndPais: Byte;
+                PtsAcum: DWord;
+              end;
+
+  const
+    NomPaises:
+      Array[1..13] of String[17] = ( 'Argentina', 'Bolivia', 'Brasil', 'Chile',
+                                     'Colombia', 'Ecuador', 'Guyana', 'Paraguay',
+                                     'Peru', 'Surinam', 'Trinidad y Tobago',
+                                     'Uruguay', 'Venezuela');
+
+    CodPaises:
+      Array[1..13] of String[3] = ('ARG', 'BOL', 'BRA', 'CHL', 'COL', 'ECU', 'GUY',
+                                   'PRY', 'PER', 'SUR', 'TTO', 'URY', 'VEN');
+
+
+// Privado ---------------------------------------------------------------------
+implementation
+
+end.
+
