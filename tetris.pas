6bdcb248baffdd1dc23642ca935e0c8a9255d8bf
gabrielmj23 <gabrielmj2312@gmail.com>
2022-06-21 19:23:50 -0400

    Agregar archivos iniciales del formulario

---
+++ tetris.pas
@@ -0,0 +1,27 @@
+unit Tetris;
+{$mode objfpc}{$H+}
+
+interface
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls;
+
+type
+
+  { TJuego }
+
+  TJuego = class(TForm)
+    Pantallas: TPageControl;
+  private
+
+  public
+
+  end;
+
+var
+  Juego: TJuego;
+
+implementation
+{$R *.lfm}
+
+end.
+
