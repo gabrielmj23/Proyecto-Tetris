9054c064ef35215a81abd1fff84390b39c73edd1
Gabriel Méndez <75638692+gabrielmj23@users.noreply.github.com>
2022-07-11 19:09:35 -0400

    Avances del juego principal y archivos de reportes

--- formtetris.pas
+++ formtetris.pas
@@ -37,23 +37,31 @@ type
     Image4: TImage;
     Image5: TImage;
     i_USUARIO: TImage;
+    FondoJuego: TImage;
     ImgODS: TImage;
     ImgSigPieza: TImage;
+    NRest: TLabel;
+    BordeCont: TShape;
+    TimerJuego: TTimer;
+    TxtRestante: TLabel;
     MensajeODS: TLabel;
+    TituloODS: TLabel;
+    TxtSiguiente: TLabel;
+    TextoPuntaje: TLabel;
+    Pantallas: TPageControl;
     PantallaInicial: TTabSheet;
     inicioSesion: TTabSheet;
     Registracion: TTabSheet;
     Estadisticas: TTabSheet;
-    TxtSiguiente: TLabel;
-    TextoPuntaje: TLabel;
-    Pantallas: TPageControl;
     PantJuego: TTabSheet;
     DatosJug: TLabel;
     procedure BInicioSesionClick(Sender: TObject);
     procedure BRegistrarseClick(Sender: TObject);
     procedure BvolverClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
     procedure ListaPaisesItemClick(Sender: TObject; Index: integer);
     procedure PantJuegoShow(Sender: TObject);
+    procedure TimerJuegoTimer(Sender: TObject);
   private
 
   public
@@ -74,7 +82,42 @@ var
   Tablero: Array[1..12, 1..9] of Byte;
   GrafTablero: Array[1..12, 1..9] of TImage;
   Puntaje: DWord;
-  IdSigPieza: Byte;
+  IdActPieza, IdSigPieza: Byte;
+  ModoJ: Char;
+  CtRest: Integer;
+  JuegoActivo: Boolean;
+
+procedure TTetris.FormCreate(Sender: TObject);
+begin
+  Pantallas.ActivePageIndex := 0;
+end;
+
+procedure TTetris.BInicioSesionClick(Sender: TObject);
+begin
+ inicioSesion.show;
+end;
+
+procedure TTetris.BRegistrarseClick(Sender: TObject);
+begin
+  registracion.show;
+end;
+
+procedure TTetris.BvolverClick(Sender: TObject);
+begin
+ PantallaInicial.show;
+end;
+
+procedure TTetris.ListaPaisesItemClick(Sender: TObject; Index: integer);
+  var
+  Contador : integer;
+begin
+  ListaPaises:=TCheckListBox(Sender);
+  for Contador:= 0 to ListaPaises.Items.Count-1 do
+    begin
+      if (Contador<>Index) then
+        ListaPaises.Checked[Contador]:= false;
+    end;
+end;
 
 procedure MostrarCeldas();
 const
@@ -113,25 +156,44 @@ begin
       end;
 end;
 
-procedure MostrarSigPieza();
+procedure GenPiezas(VAR IdAct, IdSig: Byte);
 var
-  ArchivoImg: String;
+  ArchSig: String;
 begin
-  ArchivoImg := 'img/piezas/';
-  // Seleccionar archivo
-  case IdSigPieza of
-    1: ArchivoImg := ArchivoImg + 'pieza1.png';
-    2: ArchivoImg := ArchivoImg + 'pieza2.png';
-    3: ArchivoImg := ArchivoImg + 'pieza3.png';
-    4: ArchivoImg := ArchivoImg + 'pieza4.png';
-    5: ArchivoImg := ArchivoImg + 'pieza5.png';
-    6: ArchivoImg := ArchivoImg + 'pieza6.png';
-    7: ArchivoImg := ArchivoImg + 'pieza7.png';
-    8: ArchivoImg := ArchivoImg + 'pieza8.png';
-  end;
-  // Cargar imagen
-  Tetris.ImgSigPieza.Picture.LoadFromFile(ArchivoImg);
-  // PENDIENTE: Mostrar ODS asociado con su imagen respectiva
+  if CtRest = 0 then
+    JuegoActivo := False
+  else
+    begin
+      CtRest := CtRest - 1;
+      Tetris.NRest.Caption := intToStr(CtRest);
+      if IdAct = 0 then
+        // Generar pieza actual de forma aleatoria
+        IdAct := random(8) + 1
+      else
+        // Actualizar pieza actual por la seleccionada anteriormente
+        IdAct := IdSig;
+      IdSig := random(8) + 1;
+
+      // Seleccionar archivos de imagen de la siguiente pieza
+      ArchSig := 'img/piezas/';
+      case IdSig of
+        1: ArchSig := ArchSig + 'pieza1.png';
+        2: ArchSig := ArchSig + 'pieza2.png';
+        3: ArchSig := ArchSig + 'pieza3.png';
+        4: ArchSig := ArchSig + 'pieza4.png';
+        5: ArchSig := ArchSig + 'pieza5.png';
+        6: ArchSig := ArchSig + 'pieza6.png';
+        7: ArchSig := ArchSig + 'pieza7.png';
+        8: ArchSig := ArchSig + 'pieza8.png';
+      end;
+      // Cargar imagen de siguiente pieza
+      Tetris.ImgSigPieza.Picture.LoadFromFile(ArchSig);
+
+      // Cargar datos del ODS de la pieza actual
+      Tetris.ImgODS.Picture.LoadFromFile(ImagenesODS[IdAct]);
+      Tetris.TituloODS.Caption := TitulosODS[IdAct];
+      Tetris.MensajeODS.Caption := MensajesODS[IdAct];
+    end;
 end;
 
 function LimpiarFilas(): DWord;
@@ -174,67 +236,55 @@ var
   Formato: String;
   i, j: Byte;
 begin
+  JuegoActivo := True;
+  // Cargar imagen de fondo
+  FondoJuego.Picture.LoadFromFile('img/imgFondo.jpg');
   // PARA PRUEBAS: Inicializar jugador
   with JugActual do
     begin
       NombreComp := 'Nombre';
-      USUARIO := 'Usuario';
+      Usuario := 'Usuario';
       IndPais := 3;
       Formato := 'Jugador: %0:-S (%1:-S)   Pais: %2:-S   Puntos acumulados: %3-D';
-      DatosJug.Caption := Format(Formato, [NombreComp, USUARIO, CodPaises[IndPais], puntosJugador(USUARIO)]);
+      DatosJug.Caption := Format(Formato, [NombreComp, Usuario, CodPaises[IndPais], puntosJugador(Usuario)]);
     end;
+  // Mostrar tiempo o contador de jugadas
+  ModoJ := 'T';
+  case ModoJ of
+    'T': begin
+           TxtRestante.Caption := 'Tiempo restante:';
+           CtRest := 10;
+           NRest.Caption := '10';
+           TimerJuego.Enabled := True;
+         end;
+    'J': begin
+           TxtRestante.Caption := 'Jugadas restantes:';
+           CtRest := 10;
+           NRest.Caption := '10';
+         end;
+  end;
   // Limpiar tablero de Tetris y mostrar
   for i := 1 to 12 do
     for j := 1 to 9 do
-        Tablero[i,j] := 1;
-  Tablero[1,1] := 2;
-  Tablero[1,2] := 3;
-  Tablero[1,3] := 4;
-  Tablero[1,4] := 5;
-  Tablero[1,5] := 6;
-  Tablero[1,6] := 7;
-  Tablero[1,7] := 8;
-  Tablero[1,8] := 0;
+      Tablero[i,j] := 0;
   MostrarCeldas();
   //Inicializar puntaje
   Puntaje := 0;
   TextoPuntaje.Caption := 'Puntaje: ' + IntToStr(Puntaje);
   // PRUEBAS: Generar primera pieza y siguiente pieza
-  IdSigPieza := 8;
-  MostrarSigPieza();
+  randomize;
+  IdActPieza := 0;
+  IdSigPieza := 0;
+  GenPiezas(IdActPieza, IdSigPieza);
 end;
 
-procedure TTetris.BInicioSesionClick(Sender: TObject);
+procedure TTetris.TimerJuegoTimer(Sender: TObject);
 begin
- inicioSesion.show;
+  NRest.Caption := intToStr(CtRest);
+  CtRest := CtRest - 1;
+  if CtRest = -1 then
+    TimerJuego.Enabled := False;
 end;
-
-procedure TTetris.BRegistrarseClick(Sender: TObject);
-begin
-  registracion.show;
-end;
-
-procedure TTetris.BvolverClick(Sender: TObject);
-begin
- PantallaInicial.show;
-end;
-
-procedure TTetris.ListaPaisesItemClick(Sender: TObject; Index: integer);
-  var
-  Contador : integer;
-begin
-  ListaPaises:=TCheckListBox(Sender);
-  for Contador:= 0 to ListaPaises.Items.Count-1 do
-    begin
-      if (Contador<>Index) then
-        ListaPaises.Checked[Contador]:= false;
-    end;
-end;
-
-
-
-
 
 
 end.
-
