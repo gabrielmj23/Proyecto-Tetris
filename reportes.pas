eab8648f472709e34bff27c5cb124ddd3b1437db
Gabriel Méndez <75638692+gabrielmj23@users.noreply.github.com>
2022-07-11 19:37:19 -0400

    Archivos de reportes (ahora si)

--- reportes.pas
+++ reportes.pas
@@ -2,11 +2,23 @@ unit reportes;
 
 // Público ---------------------------------------------------------------------
 interface
+  uses tiposYConst;
+
   function puntosJugador(Usuario: String): DWord;
+
+  function reporteGlobal(Directorio: String): RepGeneral;
+
+  function reportePais(Directorio, Pais: String): RepGeneral;
+
+  function reporteJugador(Directorio, UsuarioJ: String): RepJugador;
+
+  function topGlobal(Directorio: String): RepTop5;
+
+  function topPais(Directorio, Pais: String): RepTop5;
 
 // Privado ---------------------------------------------------------------------
 implementation
-  uses tiposYConst, sysutils;
+  uses sysutils;
 
   function puntosJugador(Usuario: String): DWord;
   var
@@ -33,5 +45,453 @@ implementation
     closeFile(Juegos);
   end;
 
-end.
+  // Función que rellena extremos de un mensaje para centrarlo
+  function llenarBlancos(Mensaje: String; Longitud: Byte): String;
+  var
+    i, TotBlancos, AColocar: Byte;
+  begin
+    TotBlancos := Longitud - length(Mensaje);
+    AColocar := TotBlancos div 2;
+    llenarBlancos := '';
+    // Agregar espacios a la izquierda
+    for i := 1 to AColocar do
+      llenarBlancos := llenarBlancos + ' ';
 
+    // Agregar mensaje de entrada
+    llenarBlancos := llenarBlancos + Mensaje;
+
+    if TotBlancos mod 2 <> 0 then
+      AColocar := AColocar + 1;
+    // Agregar espacios a la derecha
+    for i := 1 to AColocar do
+      llenarBlancos := llenarBlancos + ' ';
+  end;
+
+  function reporteGlobal(Directorio: String): RepGeneral;
+  var
+    CantJugadores, i: Word;
+    Jugadores: File of Jugador;
+    Reporte: Text;
+    NomArch: String;
+    JActual: Jugador;
+    PtsJug: DWord;
+  begin
+    // Verificar que el archivo existe
+    if not fileExists('Jugadores.dat') then
+      begin
+        setLength(reporteGlobal.LJugadores, 0);
+        setLength(reporteGlobal.LPuntajes, 0);
+        reporteGlobal.NArchivo := '';
+      end
+    else
+      begin
+        // Abrir archivo de jugadores
+        assignFile(Jugadores, 'Jugadores.dat');
+        reset(Jugadores);
+        // Establecer tamaños de arreglos
+        CantJugadores := fileSize(Jugadores);
+        setLength(reporteGlobal.LJugadores, CantJugadores);
+        setLength(reporteGlobal.LPuntajes, CantJugadores);
+        // Abrir archivo de reporte
+        NomArch := Directorio + '\ReporteGlobal_' + FormatDateTime('dd-mm-yyyy_hh-mm', Now) + '.txt';
+        reporteGlobal.NArchivo := NomArch;
+        assignFile(Reporte, NomArch);
+        rewrite(Reporte);
+
+        // Escribir presentación de archivo
+        write(Reporte, '--------------------------------------------------- REPORTE GLOBAL -------------------------- ');
+        writeln(Reporte, FormatDateTime('dd/mm/yyyy hh:mm', Now));
+        writeln(Reporte, '');
+        writeln(Reporte, '         NOMBRE            |    USUARIO    |             CORREO             |     PAIS      |    PUNTAJE');
+
+        // Recorrer archivo de jugadores, escribir y almacenar
+        i := 0;
+        while not eof(Jugadores) do
+          begin
+            read(Jugadores, JActual);
+            PtsJug := puntosJugador(JActual.Usuario);
+            // Almacenar en registro para usar en formulario
+            reporteGlobal.LJugadores[i] := JActual;
+            reporteGlobal.LPuntajes[i] := PtsJug;
+            i := i+1;
+            // Escribir al archivo de reporte
+            with JActual do
+              begin
+                write(Reporte, llenarBlancos(NombreComp, 27) + '|');
+                write(Reporte, llenarBlancos(Usuario, 15) + '|');
+                write(Reporte, llenarBlancos(Correo, 32) + '|');
+                write(Reporte, llenarBlancos(NomPaises[IndPais], 15) + '|');
+                writeln(Reporte, llenarBlancos(IntToStr(PtsJug), 15));
+              end;
+          end;
+
+        // Cerrar archivos
+        closeFile(Reporte);
+        closeFile(Jugadores);
+      end;
+  end;
+
+  function reportePais(Directorio, Pais: String): RepGeneral;
+  var
+    CantJugadores, i: Word;
+    Jugadores: File of Jugador;
+    Reporte: Text;
+    NomArch: String;
+    JActual: Jugador;
+    PtsJug: DWord;
+  begin
+    // Verificar que el archivo existe
+    if not fileExists('Jugadores.dat') then
+      begin
+        setLength(reportePais.LJugadores, 0);
+        setLength(reportePais.LPuntajes, 0);
+        reportePais.NArchivo := '';
+      end
+    else
+      begin
+        // Abrir archivo de jugadores
+        assignFile(Jugadores, 'Jugadores.dat');
+        reset(Jugadores);
+        // Establecer tamaño inicial de arreglos
+        CantJugadores := fileSize(Jugadores);
+        setLength(reportePais.LJugadores, CantJugadores);
+        setLength(reportePais.LPuntajes, CantJugadores);
+        // Abrir archivo de reporte
+        NomArch := Directorio + '\Reporte' + Pais + '_' + FormatDateTime('dd-mm-yyyy_hh-mm', Now) + '.txt';
+        reportePais.NArchivo := NomArch;
+        assignFile(Reporte, NomArch);
+        rewrite(Reporte);
+
+        // Escribir presentación de archivo
+        write(Reporte, '------------------------------ REPORTE DE ' + UpperCase(Pais) + ' ----------------------------- ');
+        writeln(Reporte, FormatDateTime('dd/mm/yyyy hh:mm', Now));
+        writeln(Reporte, '');
+        writeln(Reporte, '          NOMBRE          |    USUARIO    |             CORREO             |    PUNTAJE');
+
+        // Recorrer archivo de jugadores, escribir y almacenar los del pais especificado
+        i := 0;
+        while not eof(Jugadores) do
+          begin
+            read(Jugadores, JActual);
+            if NomPaises[JActual.IndPais] = Pais then
+              begin
+                // El país es correcto
+                PtsJug := puntosJugador(JActual.Usuario);
+                reportePais.LJugadores[i] := JActual;
+                reportePais.LPuntajes[i] := PtsJug;
+                i := i+1;
+                // Escribir en reporte
+                with JActual do
+                  begin
+                    write(Reporte, llenarBlancos(NombreComp, 27) + '|');
+                    write(Reporte, llenarBlancos(Usuario, 15) + '|');
+                    write(Reporte, llenarBlancos(Correo, 32) + '|');
+                    writeln(Reporte, llenarBlancos(IntToStr(PtsJug), 15));
+                  end;
+              end;
+          end;
+        // Ajustar longitudes de arreglos
+        setLength(reportePais.LJugadores, i);
+        setLength(reportePais.LPuntajes, i);
+
+        // Cerrar archivos
+        closeFile(Reporte);
+        closeFile(Jugadores);
+      end;
+  end;
+
+  function reporteJugador(Directorio, UsuarioJ: String): RepJugador;
+  var
+    CantJuegos, i: Word;
+    NomArch: String;
+    Jugadores: File of Jugador;
+    JugActual: Jugador;
+    Juegos: File of Juego;
+    JuegoActual: Juego;
+    Reporte: Text;
+    Existe: Boolean;
+  begin
+    // Verificar que el archivo existe
+    if not fileExists('Jugadores.dat') then
+      begin
+        setLength(reporteJugador.LJuegos, 0);
+        reporteJugador.NArchivo := '';
+      end
+    else
+      begin
+        // Abrir archivo de jugadores
+        assignFile(Jugadores, 'Jugadores.dat');
+        reset(Jugadores);
+        // Buscar que exista el jugador
+        Existe := False;
+        while (not eof(Jugadores)) and (not Existe) do
+          begin
+            read(Jugadores, JugActual);
+            if JugActual.Usuario = UsuarioJ then
+              begin
+                Existe := True;
+                reporteJugador.JugSolicitado := JugActual;
+              end;
+          end;
+        reporteJugador.JugExiste := Existe;
+        setLength(reporteJugador.LJuegos, 0);
+        closeFile(Jugadores);
+
+        if Existe then
+          begin
+            // Abrir archivo de reporte
+            NomArch := Directorio + '\ReporteJugador_' + FormatDateTime('dd-mm-yyyy_hh-mm', Now) + '.txt';
+            reporteJugador.NArchivo := NomArch;
+            assignFile(Reporte, NomArch);
+            rewrite(Reporte);
+
+            // Escribir presentación de reporte
+            write(Reporte, '----------------------------------------------- REPORTE DE JUGADOR -------------------------- ');
+            writeln(Reporte, FormatDateTime('dd/mm/yyyy hh:mm', Now));
+            writeln(Reporte, '');
+            writeln(Reporte, '         NOMBRE            |    USUARIO    |             CORREO             |     PAIS      |    PUNTAJE');
+            // Escribir datos de jugador
+            write(Reporte, llenarBlancos(JugActual.NombreComp, 27) + '|');
+            write(Reporte, llenarBlancos(JugActual.Usuario, 15) + '|');
+            write(Reporte, llenarBlancos(JugActual.Correo, 32) + '|');
+            write(Reporte, llenarBlancos(NomPaises[JugActual.IndPais], 15) + '|');
+            writeln(Reporte, llenarBlancos(IntToStr(puntosJugador(JugActual.Usuario)), 15));
+            writeln(Reporte, '');
+            writeln(Reporte, '                               --- PARTIDAS JUGADAS ---');
+
+            // Verificar que existe el archivo de juegos y abrirlo
+            CantJuegos := 0;
+            if fileExists('Juegos.dat') then
+              begin
+                assignFile(Juegos, 'Juegos.dat');
+                reset(Juegos);
+                // Contar juegos del usuario ingresado
+                while not eof(Juegos) do
+                  begin
+                    read(Juegos, JuegoActual);
+                    if JuegoActual.Usuario = UsuarioJ then
+                      CantJuegos := CantJuegos + 1;
+                  end;
+                closeFile(Juegos);
+
+                if CantJuegos > 0 then
+                  begin
+                    // El usuario ha jugado. Volver a recorrer archivo de juegos y mostrar en reporte
+                    writeln(Reporte, '                  CODIGO      |     FECHA     |   HORA   |    PUNTAJE');
+                    reset(Juegos);
+                    i := 0;
+                    setLength(reporteJugador.LJuegos, CantJuegos);
+                    while not eof(Juegos) do
+                      begin
+                        read(Juegos, JuegoActual);
+                        if JuegoActual.Usuario = UsuarioJ then
+                          begin
+                            with JuegoActual do
+                              begin
+                                write(Reporte, '            ' + llenarBlancos(IntToStr(Codigo), 18) + '|');
+                                write(Reporte, llenarBlancos(Fecha, 15) + '|');
+                                write(Reporte, llenarBlancos(Hora, 10) + '|');
+                                writeln(Reporte, llenarBlancos(IntToStr(PtsGanados), 15));
+                              end;
+                            // Agregar a arreglo de juegos para el registro
+                            reporteJugador.LJuegos[i] := JuegoActual;
+                            i := i+1;
+                          end;
+                      end;
+                    closeFile(Juegos);
+                  end;
+              end;
+            // Indicar si no se han jugado juegos (en general o solo el usuario)
+            if CantJuegos = 0 then
+              write(Reporte, '                                El usuario no ha jugado');
+            closeFile(Reporte);
+          end;
+      end;
+  end;
+
+  function topGlobal(Directorio: String): RepTop5;
+  var
+    Jugadores: File of Jugador;
+    Reporte: Text;
+    CtJugadores, PtsJug: DWord;
+    NomArch: String;
+    i, j: Byte;
+    JugActual: Jugador;
+    JugEntra: Boolean;
+  begin
+    CtJugadores := 0;
+    // Verificar que el archivo existe
+    if not fileExists('Jugadores.dat') then
+      begin
+        setLength(topGlobal.Top, 0);
+        setLength(topGlobal.LPuntajes, 0);
+      end
+    else
+      begin
+        // Abrir archivo de jugadores y determinar cantidad
+        assignFile(Jugadores, 'Jugadores.dat');
+        reset(Jugadores);
+        CtJugadores := fileSize(Jugadores);
+        if CtJugadores > 5 then
+          CtJugadores := 5;
+        topGlobal.CantJugadores := CtJugadores;
+
+        // Abrir archivo de reporte y escribir presentación
+        NomArch := Directorio + '\Top5Global_' + FormatDateTime('dd-mm-yyyy_hh-mm', Now) + '.txt';
+        topGlobal.NArchivo := NomArch;
+        assignFile(Reporte, NomArch);
+        rewrite(Reporte);
+        write(Reporte, '------------------------------------------------- TOP 5 GLOBAL -------------------------------------- ');
+        writeln(Reporte, FormatDateTime('dd/mm/yyyy hh:mm', Now));
+        writeln(Reporte, '');
+        // Escribir mensaje si no hay jugadores registrados
+        if CtJugadores = 0 then
+          write(Reporte, '                                No hay jugadores disponibles')
+        else
+          begin
+            // Inicializar arreglos
+            setLength(topGlobal.Top, CtJugadores);
+            setLength(topGlobal.LPuntajes, CtJugadores);
+            for i := 1 to CtJugadores do
+              topGlobal.LPuntajes[i] := 0;
+            // Recorrer archivo de jugadores y crear top
+            while not eof(Jugadores) do
+              begin
+                read(Jugadores, JugActual);
+                i := 1;
+                JugEntra := False;
+                PtsJug := puntosJugador(JugActual.Usuario);
+                while (i <= CtJugadores) and (not JugEntra) do
+                  begin
+                    if PtsJug >= topGlobal.LPuntajes[i] then
+                      begin
+                        JugEntra := True;
+                        for j := CtJugadores downto i+1 do
+                          begin
+                            // Bajar jugadores del top
+                            topGlobal.Top[j] := topGlobal.Top[j-1];
+                            topGlobal.LPuntajes[j] := topGlobal.LPuntajes[j-1];
+                          end;
+                        // Actualizar posición en la que se inserta el jugador
+                        topGlobal.Top[i] := JugActual;
+                        topGlobal.LPuntajes[i] := PtsJug;
+                      end;
+                    i := i+1;
+                  end;
+              end;
+            closeFile(Jugadores);
+            // Escribir top en el reporte
+            writeln(Reporte, ' POSICION |         NOMBRE            |    USUARIO    |             CORREO             |     PAIS      |    PUNTAJE');
+            for i := 1 to CtJugadores do
+              with topGlobal.Top[i] do
+                begin
+                  write(Reporte, llenarBlancos(IntToStr(i), 10) + '|');
+                  write(Reporte, llenarBlancos(NombreComp, 27) + '|');
+                  write(Reporte, llenarBlancos(Usuario, 15) + '|');
+                  write(Reporte, llenarBlancos(Correo, 32) + '|');
+                  write(Reporte, llenarBlancos(NomPaises[IndPais], 15) + '|');
+                  writeln(Reporte, llenarBlancos(intToStr(puntosJugador(Usuario)), 15));
+                end;
+            if CtJugadores < 5 then
+              write(Reporte, '                              No hay suficientes para un top 5');
+          end;
+        // Cerrar reporte
+        closeFile(Reporte);
+      end;
+  end;
+
+  function topPais(Directorio, Pais: String): RepTop5;
+  var
+    Jugadores: File of Jugador;
+    Reporte: Text;
+    CtJugadores, PtsJug, i, j: DWord;
+    JugAct: Jugador;
+    NomArch: String;
+    JugEntra: Boolean;
+  begin
+    // Verificar que el archivo exista
+    if not fileExists('Jugadores.dat') then
+      begin
+        setLength(topPais.Top, 0);
+        setLength(topPais.LPuntajes, 0);
+      end
+    else
+      begin
+        // Abrir archivo de jugadores y contar los del país
+        CtJugadores := 0;
+        assignFile(Jugadores, 'Jugadores.dat');
+        reset(Jugadores);
+        while not eof(Jugadores) do
+          begin
+            read(Jugadores, JugAct);
+            if NomPaises[JugAct.IndPais] = Pais then
+              CtJugadores := CtJugadores + 1;
+          end;
+        if CtJugadores > 5 then
+          CtJugadores := 5;
+        topPais.CantJugadores := CtJugadores;
+        closeFile(Jugadores);
+        // Abrir archivo de reporte y escribir presentación
+        NomArch := Directorio + '\Top5' + Pais + '_' + FormatDateTime('dd-mm-yyyy_hh-mm', Now) + '.txt';
+        topPais.NArchivo := NomArch;
+        assignFile(Reporte, NomArch);
+        rewrite(Reporte);
+        write(Reporte, '-------------------------------------- TOP 5 ' + UpperCase(Pais) + ' ------------------------------ ');
+        writeln(Reporte, FormatDateTime('dd/mm/yyyy hh:mm', Now));
+        writeln(Reporte, '');
+        // Escribir mensaje si el país no tiene jugadores
+        if CtJugadores = 0 then
+          write(Reporte, '                                  Este país no tiene jugadores')
+        else
+          begin
+            // Inicializar arreglos
+            setLength(topPais.Top, CtJugadores);
+            setLength(topPais.LPuntajes, CtJugadores);
+            for i := 1 to CtJugadores do
+              topPais.LPuntajes[i] := 0;
+            // Abrir archivo de jugadores y recorrerlo para generar top
+            reset(Jugadores);
+            while not eof(Jugadores) do
+              begin
+                read(Jugadores, JugAct);
+                i := 1;
+                JugEntra := False;
+                PtsJug := puntosJugador(JugAct.Usuario);
+                while (i <= CtJugadores) and (not JugEntra) do
+                  begin
+                    if PtsJug >= topPais.LPuntajes[i] then
+                      begin
+                        for j := CtJugadores downto i+1 do
+                          begin
+                            // Bajar jugadores del top
+                            topPais.Top[j] := topPais.Top[j-1];
+                            topPais.LPuntajes[j] := topPais.LPuntajes[j-1];
+                          end;
+                        // Actualizar posición en la que se inserta el jugador
+                        topPais.Top[i] := JugAct;
+                        topPais.LPuntajes[i] := PtsJug;
+                      end;
+                    i := i+1;
+                  end;
+              end;
+            closeFile(Jugadores);
+            // Escribir top en el reporte
+            writeln(Reporte, ' POSICION |          NOMBRE          |    USUARIO    |             CORREO             |    PUNTAJE');
+            for i := 1 to CtJugadores do
+              with topPais.Top[i] do
+                begin
+                  write(Reporte, llenarBlancos(IntToStr(i), 10) + '|');
+                  write(Reporte, llenarBlancos(NombreComp, 27) + '|');
+                  write(Reporte, llenarBlancos(Usuario, 15) + '|');
+                  write(Reporte, llenarBlancos(Correo, 32) + '|');
+                  writeln(Reporte, llenarBlancos(intToStr(puntosJugador(Usuario)), 15));
+                end;
+            if CtJugadores < 5 then
+              write(Reporte, '                               No hay suficientes para un top 5');
+          end;
+        // Cerrar reporte
+        closeFile(Reporte);
+      end;
+  end;
+
+end.
--- tiposyconst.pas
+++ tiposyconst.pas
@@ -19,20 +19,98 @@ interface
                 IndPais: Byte;
               end;
 
+    // Para reporte general global y de pais
+    RepGeneral = Record
+                   LJugadores: Array of Jugador;
+                   LPuntajes: Array of DWord;
+                   NArchivo: String;
+                 end;
+
+    // Para reporte de jugador
+    RepJugador = Record
+                   JugSolicitado: Jugador;
+                   LJuegos: Array of Juego;
+                   NArchivo: String;
+                   JugExiste: Boolean;
+                 end;
+
+    // Para reportes de top 5
+    RepTop5 = Record
+                Top: Array of Jugador;
+                CantJugadores: Byte;  // En caso de que no hallan 5 jugadores en primer lugar
+                LPuntajes: Array of DWord;
+                NArchivo: String;
+              end;
+
   const
+    // PAISES - PARA EL REGISTRO, JUEGO Y REPORTES
     NomPaises:
       Array[1..13] of String[17] = ( 'Argentina', 'Bolivia', 'Brasil', 'Chile',
                                      'Colombia', 'Ecuador', 'Guyana', 'Paraguay',
                                      'Peru', 'Surinam', 'Trinidad y Tobago',
                                      'Uruguay', 'Venezuela');
-
     CodPaises:
       Array[1..13] of String[3] = ('ARG', 'BOL', 'BRA', 'CHL', 'COL', 'ECU', 'GUY',
                                    'PRY', 'PER', 'SUR', 'TTO', 'URY', 'VEN');
+
+    // ODS - PARA EL JUEGO PRINCIPAL
+    TitulosODS:
+      Array[1..17] of String[32] = ('FIN DE LA POBREZA',
+                                    'HAMBRE CERO',
+                                    'SALUD Y BIENESTAR',
+                                    'EDUCACION DE CALIDAD',
+                                    'IGUALDAD DE GENERO',
+                                    'AGUA LIMPIA',
+                                    'ENERGIA ASEQUIBLE Y LIMPIA',
+                                    'TRABAJO DECENTE',
+                                    'INDUSTRIA E INFRAESTRUCTURA',
+                                    'REDUCCION DE DESIGUALDADES',
+                                    'COMUNIDADES SOSTENIBLES',
+                                    'PRODUCCION Y CONSUMO RESPONSABLE',
+                                    'ACCION POR EL CLIMA',
+                                    'VIDA SUBMARINA',
+                                    'VIDA TERRESTRE',
+                                    'PAZ Y JUSTICIA',
+                                    'ALIANZAS');
+    MensajesODS:
+      Array[1..17] of String[67] = ('Todos merecemos los recursos para una vida digna',
+                                    'Acabemos con el hambre y la desnutricion en el mundo',
+                                    'Luchemos por la cobertura universal de salud',
+                                    'La educacion es el motor del desarrollo sostenible',
+                                    'El crecimiento se da sin discriminacion de genero',
+                                    'El agua potable es indispensable para la vida',
+                                    'La energia limpia nos ayuda a nosotros y al medio ambiente',
+                                    'Los trabajadores merecen condiciones adecuadas',
+                                    'La infraestructura es vital para el desarrollo social y tecnologico',
+                                    'Todos merecemos acceso a los mismos recursos basicos',
+                                    'Nuestras ciudades pueden ser mas limpias y sostenibles',
+                                    'Reciclar y reducir desechos ayuda a disminuir la huella ecologica',
+                                    'Podemos adaptarnos y luchar contra el cambio climatico',
+                                    'Debemos proteger los ecosistemas marinos',
+                                    'Es urgente reducir la perdida de biodiversidad',
+                                    'Cuidar los derechos humanos es cuidar a la humanidad',
+                                    'Nada se lograra sin trabajo en equipo');
+    ImagenesODS:
+      Array[1..17] of String[27] = ('img/ods/pobreza.png',
+                                    'img/ods/hambrecero.png',
+                                    'img/ods/salud.png',
+                                    'img/ods/educacion.png',
+                                    'img/ods/iggenero.png',
+                                    'img/ods/agua.png',
+                                    'img/ods/energia.png',
+                                    'img/ods/trabajo.png',
+                                    'img/ods/infraestructura.png',
+                                    'img/ods/desigualdad.png',
+                                    'img/ods/comsostenibles.png',
+                                    'img/ods/consresponsable.png',
+                                    'img/ods/clima.png',
+                                    'img/ods/vsubmarina.png',
+                                    'img/ods/vterrestre.png',
+                                    'img/ods/pazyjusticia.png',
+                                    'img/ods/alianzas.png');
 
 
 // Privado ---------------------------------------------------------------------
 implementation
 
 end.
-
